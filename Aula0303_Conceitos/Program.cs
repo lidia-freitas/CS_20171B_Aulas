d58e29c3f3ed39ddb96f255aefadc1084395e7c4
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2017-03-03 21:57:57 -0300

    Versao completa

--- Aula0303_Conceitos/Program.cs
+++ Aula0303_Conceitos/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static Aula0303_Conceitos.Util;
 
 namespace Aula0303_Conceitos
 {
@@ -10,6 +7,46 @@ namespace Aula0303_Conceitos
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("Hello world!");
+
+            MeioTransporte mt = new Barco();
+            ((Barco)mt).QuantidadePes = 10;
+
+            TipoMeioTransporte tipo = (TipoMeioTransporte) Menu();
+
+            switch (tipo)
+            {
+                case TipoMeioTransporte.Carro:
+                    Carro c = new Carro("Fusca");
+                    c.Ano = 1978;
+                    c.Capacidade = 4;
+                    c.Acelerar();
+                    c.Frear();
+                    c.Parar();
+                    break;
+                case TipoMeioTransporte.Bicicleta:
+                    Bicicleta b = new Bicicleta();
+                    b.Nome = "Caloi";
+                    b.Ano = 2016;
+                    b.Capacidade = 1;
+                    b.QuantidadeMarcha = 21;
+                    b.Parar();
+                    break;
+                case TipoMeioTransporte.Barco:
+                    Barco ba = new Barco();
+                    ba.Ano = 1969;
+                    ba.Nome = "Titanic";
+                    ba.QuantidadePes = 50;
+                    ba.Capacidade = 2000;
+                    ((IMotorizado)ba).Acelerar();
+                    ((IMotorizado)ba).Frear();
+                    ba.Parar();
+                    break;
+                default:
+                    break;
+            }
+
+            Console.ReadKey();
         }
     }
 }
