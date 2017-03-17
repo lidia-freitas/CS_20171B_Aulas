81d5d4eca1cae5245e984ee59d4bab51776785ad
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2017-03-17 20:43:23 -0300

    Nova versão do projeto em camadas

---
+++ ProjetoEmCamadas/Controllers/ClientesController.cs
@@ -0,0 +1,21 @@
+﻿using Models;
+using System.Collections.Generic;
+
+namespace Controllers
+{
+    public class ClientesController
+    {
+        private static List<Cliente> listaClientes = new List<Cliente>();
+
+        public void Adicionar(string nome, string cpf)
+        {
+            Cliente cli = new Cliente();
+            cli.Nome = nome;
+            cli.Cpf = cpf;
+
+            listaClientes.Add(cli);
+        }
+
+
+    }
+}
